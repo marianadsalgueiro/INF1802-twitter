077eebdec5e0209f1c4165847c6ff1e93657dd12
marianadsalgueiro <marianadsalgueiro@gmail.com>
2019-05-14 10:52:15 -0300

    exercicio ainda por fazer

--- src/main/java/LifeCycleManager.java
+++ src/main/java/LifeCycleManager.java
@@ -3,8 +3,6 @@ import java.io.Serializable;
 public class LifeCycleManager implements LifecycleManager, Serializable {
     public void start(){
         Tweet t = null;
-        recebeTweets(t);
-
     }
 
     public void stop(){
